d8b08afe260e0da52a652adeba5c8f27b15f81d1
berfail <kunsangaboberfy@gmail.com>
2022-03-01 20:05:03 +0100

    circleci config file :recycle:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,11 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  if ENV['RAILS_ENV'] == 'test'
+    require 'simplecov'
+    SimpleCov.start 'rails'
+    puts 'required simplecov'
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
