18b65fec7151b438c9b017a14bc257efb498caab
berfail <kunsangaboberfy@gmail.com>
2022-03-02 08:29:41 +0100

    knapsack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,8 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-
+require 'knapsack_pro'
+KnapsackPro::Adapters::RSpecAdapter.bind
 RSpec.configure do |config|
   if ENV['RAILS_ENV'] == 'test'
     require 'simplecov'
