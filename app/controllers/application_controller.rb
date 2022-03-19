f19d79a1d76c586e1dc3be85a9119aac8b88e300
berfail <kunsangaboberfy@gmail.com>
2022-03-19 18:52:25 +0100

    :recycle: classfied controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@
 # application controller
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  
+  private
+    def record_not_found
+      render plain: "404 Not Found", status: 404
+    end
 end
